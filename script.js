04a353de083bca57db70d56f062aae504026e86f
Rashel124 <webcoderitrashel124@gmail.com>
2025-02-09 22:28:11 -0800

    Variable,let

---
+++ script.js
@@ -0,0 +1,50 @@
+
+console.log("Hello Everyone");
+
+
+// Summation...
+
+let x = 550;
+let y = 660;
+
+let sum = x+y; //90
+
+console.log(sum);
+
+// Substraction
+
+let number1 =600;
+let number2 =450;
+
+let sub =number1-number2;
+
+console.log(sub);
+
+
+//multiplication........
+
+let digit1 = 30;
+let digit2 = 50;
+
+let mul = digit1*digit2;
+
+console.log(mul);
+
+
+//division.......
+
+let data1 = 60;
+let data2 = 5;
+
+let div =data1/data2;
+
+console.log(div);
+
+//persentage (vagshes ber korar rules.....)
+
+let data3 =3;
+let data4 =2;
+
+let remainder = 3%2;
+
+console.log(remainder);
